99414cac2f732242f705a9b59891e0104a9d4276
Lisa <llvvss@icloud.com>
2024-03-11 10:11:54 -0600

    first commit

---
+++ Source Code/BMIcalculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // BMI calcultor
+    // weight (kg) / height(m)
+    // underweight < 18.5
+    // normal weight 18.5 to 24.9
+    // overweight > 25
+
+}
